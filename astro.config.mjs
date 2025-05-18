870211012da4360cc0cf3836395dc7833a3e2edf
Janybanny <github@janybanny.moe>
2025-05-18 17:33:27 +0200

    add banner and fix links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.imperialdiplomacy.com/imperialrealm'
+    site: 'https://www.imperialdiplomacy.com/',
+    base: '/imperialrealm'
 });
